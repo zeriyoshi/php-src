1a44f02c89ef2d48051c43df366050574ad79d79
zeriyoshi <zeriyoshi@gmail.com>
2021-01-05 23:02:40 +0900

    fix: remove PHP_RNG_API

--- ext/standard/rng_randominterface.c
+++ ext/standard/rng_randominterface.c
@@ -20,7 +20,7 @@
 #include "rng_randominterface.h"
 #include "rng_randominterface_arginfo.h"
 
-PHP_RNG_API zend_class_entry *rng_ce_RNG_RandomInterface;
+PHPAPI zend_class_entry *rng_ce_RNG_RandomInterface;
 
 PHP_MINIT_FUNCTION(rng_randominterface)
 {
